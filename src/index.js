b47820c721c176c8262de0ac8d3628055a3fc995
Sergey Skakun <thexfiles116@gmail.com>
2020-03-13 21:41:33 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let config = new Map();
+    bracketsConfig.forEach(element => {
+        config.set(...element);
+    });
+
+    for(let i = 0; i < str.length; i++) {
+        if(config.has(str[i])) {
+            if(config.get(str[i]) === str[i] && stack[stack.length - 1] === str[i]) {
+                stack.pop();
+            }
+            else {
+                stack.push(str[i]);
+            }
+        }
+        else {
+            if(config.get(stack[stack.length - 1]) === str[i]) {
+                stack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
